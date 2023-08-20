05a7e00f85949ae2609c32031d96096c0ff92800
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-20 16:20:02 -0400

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,10 +1,10 @@
 sudo chmod -s /usr/bin/pkexec
 sudo chmod -s $(which find)
-sudo mv $(which chattr) /var/... 
+sudo mv $(which chattr) /var/persistence
 while true; do
-    sudo /var/... -ia /root
-    sudo /var/... -ia /root/king.txt 2>/dev/null
+    sudo /var/persistence -ia /root
+    sudo /var/persistence -ia /root/king.txt 2>/dev/null
     sudo echo -n "lomarkomar" >| /root/king.txt 2>/dev/null
-    sudo /var/... +ia /root/king.txt 2>/dev/null
-    sudo /var/... +ia /root
+    sudo /var/persistence +ia /root/king.txt 2>/dev/null
+    sudo /var/persistence +ia /root
 done &
