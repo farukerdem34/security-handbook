08164c8c58b16c97b49bbe9bfe6a381382df3082
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-22 13:43:36 -0400

    Update symbolic_link.sh

--- scripts/symbolic_link.sh
+++ scripts/symbolic_link.sh
@@ -1,6 +1,6 @@
 mkdir /dev/.lokal
-cp -r /root/ /dev/.lokal/...
-cd /dev/.lokal/.../root
+cp -r /root/ /dev/.lokal/x
+cd /dev/.lokal/x/root
 rm king.txt
-echo "lomarkomar" > ...
-ln -s ... king.txt
+echo "lomarkomar" > x
+ln -s x king.txt
