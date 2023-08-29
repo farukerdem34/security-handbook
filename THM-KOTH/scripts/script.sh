3c90779473f9d2e5b70138a4bcbba98bd6d47b41
Faruk <98034193+farukerdem34@users.noreply.github.com>
2023-08-29 10:54:09 +0300

    Update script.sh

--- THM-KOTH/scripts/script.sh
+++ THM-KOTH/scripts/script.sh
@@ -22,11 +22,11 @@ while true; do
           then
                clear_tmp
      fi
-     /dev/shm/asd -ia /root
-     /dev/shm/asd -ia /root/king.txt 2>/dev/null
+     /dev/shm/asd -ia /root > /dev/null
+     /dev/shm/asd -ia /root/king.txt >/dev/null
      chmod 600 /root/king.txt
-     echo -n "lomarkomar" >| /root/king.txt 2>/dev/null
+     echo -n "lomarkomar" >| /root/king.txt >/dev/null
      chmod 200 /root/king.txt
-     /dev/shm/asd +ia /root/king.txt 2>/dev/null
-     /dev/shm/asd +ia /root
+     /dev/shm/asd +ia /root/king.txt >/dev/null
+     /dev/shm/asd +ia /root >/dev/null
 done &
